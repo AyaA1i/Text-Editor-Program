b8d0709076f71d78723673c704929fadefb4f037
AyaAli <101468593+ayaaalli@users.noreply.github.com>
2022-06-03 13:51:07 +0200

    Create CS112-203-2nd-section4-20210083-20210380-20210799-A4-TextEditorFunc.h

---
+++ CS112-203-2nd-section4-20210083-20210380-20210799-A4-TextEditorFunc.h
@@ -0,0 +1,40 @@
+// FCI - Programming 1 - 2022 - Assignment 4
+// Program Name:Text Editor.cpp
+// Last Modification Date: 05-03-2022
+// Author1 and ID and Group: Aya Ali Hassan & 20210083 & A
+// Author2 and ID and Group: Mariam Ayman Taha & 20210380 & A
+// Author3 and ID and Group: Amira Basaam Dabbak & 20210799 & A
+// Teaching Assistant: Eng. Nesma Mohamed & Eng. Yousra Ayman
+
+#ifndef TEXT_EDITOR_CS112_203_2ND_SECTION4_20210083_20210380_20210799_A4_TEXTEDITORFUNC_H
+#define TEXT_EDITOR_CS112_203_2ND_SECTION4_20210083_20210380_20210799_A4_TEXTEDITORFUNC_H
+#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include<regex>
+
+
+
+
+
+void display_menu();
+void valid_option();
+void Input_FileName();
+void Add();
+void Display();
+void Empty();
+void encrypt();
+void decrypt();
+void merge();
+void count_num_of_words();
+void count_num_of_characters();
+void count_num_of_lines();
+void search_for_word();
+void count_num_of_times_word_exist();
+void upper_case();
+void lower_case();
+void first_caps();
+void save();
+
+
+#endif //TEXT_EDITOR_CS112_203_2ND_SECTION4_20210083_20210380_20210799_A4_TEXTEDITORFUNC_H
